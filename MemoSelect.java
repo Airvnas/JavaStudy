e450ba9865a9d81a82d7281bc3a3ac0c8d8a0a92
KimJinYeop <wlsduq1229@naver.com>
2022-10-29 18:47:45 +0900

    Commit 04 이클립스에서 db에 저장된 primarykey인 idx를 검색해 단일행 반환을 통해 한개의 객체를 생성해 값을 저장하여 출력을 하고, 이름,메시지 내용은 중복될수 있기때문에 다중행 반환을 통해 반환한 객체를 어레이리스트에 저장해 리스트를 출력한다.

--- MemoSelect.java
+++ MemoSelect.java
@@ -3,7 +3,6 @@ package day02;
 import java.sql.*;
 import java.util.*;
 
-import javax.net.ssl.ManagerFactoryParameters;
 //모든 메모글을 가져와 출력하자.(글번호 내림차순)
 public class MemoSelect {
 	Connection con;
@@ -33,10 +32,114 @@ public class MemoSelect {
 	public static void main(String[] args) {
 		MemoSelect app = new MemoSelect();
 		ArrayList<MemoVO> memoList = app.selectMemoAll();
+		app.printMemo(memoList);//테이블의 모든 자료 출력
 		
-		app.printMemo(memoList);
+		
+		app = new MemoSelect();
+		Scanner sc =new Scanner(System.in);
+		System.out.println("검색할 번호를 입력하세요.");
+		int idx= sc.nextInt();
+		MemoVO vo = app.selectMemo(idx);
+		app.printMemo(vo);//검색한 idx를 단일행 받아와 출력
+		
+		app = new MemoSelect();
+		System.out.println("검색할 메시지를 입력하세요.");
+		sc.skip("\r\n");
+		String str= sc.nextLine();
+		System.out.println(str);
+		memoList = app.findMemoByMsg(str);
+		app.printMemo(memoList);//검색한 메시지를 다중행 받아와 출력
+		
+		app = new MemoSelect();
+		System.out.println("검색할 이름을 입력하세요.");
+		//sc.skip("\r\n");
+		str= sc.nextLine();
+		System.out.println(str);
+		memoList = app.findMemoByName(str);
+		app.printMemo(memoList);//검색한 이름을 다중행 받아와 출력
 		
 	}
+	//글 번호로 특정글을 가져오는 메서드
+	public MemoVO selectMemo(int idx) {
+		try {
+			con =DriverManager.getConnection(url,user,pwd);
+			String sql="SELECT * FROM memo WHERE IDX="+idx;
+			stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				int i = rs.getInt("idx");
+				if(i==idx) {
+					String name= rs.getString("name");
+					String msg = rs.getString("msg");
+					java.sql.Date wdate = rs.getDate("wdate");
+					MemoVO vo = new MemoVO(i,name,msg,wdate);
+					return vo;
+				}
+			}return null;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}finally {
+			close();
+		}
+	}//-------------------------------------
+	
+	public ArrayList<MemoVO> findMemoByMsg(String keyword){
+		try {
+			con=DriverManager.getConnection(url,user,pwd);
+			ArrayList<MemoVO> arrList= new ArrayList<>();
+			String sql="SELECT * FROM memo WHERE MSG='"+keyword+"'";
+			stmt=con.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				String msg= rs.getString("msg");
+				if(msg.equals(keyword)) {
+					String name= rs.getString("name");
+					int idx = rs.getInt("idx");
+					java.sql.Date wdate = rs.getDate("wdate");
+					MemoVO vo = new MemoVO(idx,name,msg,wdate);
+					arrList.add(vo);
+				}
+			}
+			if(!arrList.isEmpty()) return arrList;
+			else return null;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}finally {
+			close();
+		}
+	}//-------------------------------------
+	public ArrayList<MemoVO> findMemoByName(String keyword){
+		try {
+			con=DriverManager.getConnection(url,user,pwd);
+			ArrayList<MemoVO> arrList= new ArrayList<>();
+			stmt=con.createStatement();
+			String sql= "SELECT * FROM memo WHERE NAME='"+keyword+"'";
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				String name=rs.getString("name");
+				if(name.equals(keyword)) {
+					int idx=rs.getInt("idx");
+					String msg=rs.getString("msg");
+					java.sql.Date wdate=rs.getDate("wdate");
+					MemoVO vo =new MemoVO(idx,name,msg,wdate);
+					arrList.add(vo);
+				}
+			}
+			if(!arrList.isEmpty()) return arrList;
+			else return null;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}finally {
+			close();
+		}
+	}//-------------------------------------
+	
+	
 	private ArrayList<MemoVO> selectMemoAll() {
 		try {
 			con =DriverManager.getConnection(url,user,pwd);
@@ -84,8 +187,15 @@ public class MemoSelect {
 			}
 			System.out.println("---------------------------------");
 		}
-		
-		
+	}
+	private void printMemo(MemoVO memo) {
+		if(memo!=null) {
+			System.out.println("---------------------------------");
+			System.out.println("\t글번호\t작성자명\t메모내용\t\t작성일");
+			System.out.println("---------------------------------");
+			System.out.println("\t"+memo.getIdx()+"\t"+memo.getName()+"\t"+memo.getMsg()+"\t\t"+memo.getWdate()); 
+			System.out.println("---------------------------------");
+		}
 	}
 
 
