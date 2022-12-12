20d7601b78da3a429337d52501e1bd564393e0ff
KimJinYeop <wlsduq1229@naver.com>
2022-12-12 17:41:47 +0900

    Commint 38
    jsp파일에 있는 java코드가 복잡하기 때문에 이를 java파일로 만들어 include시켜 jsp파일을 단순하게 만들었고, 게시글의 검색처리와 페이지당 보여줄 게시글의 수를 select를 통해 입력받아 출력하도록 했다. 검색처리는 mybatis의 if문을 이용해 입력이 있을때, 없을때를 게시글 출력하는 sql문 하나로 모두 수행할 수 있었다.

--- SpringMVC02/src/main/java/com/board/model/PagingVO.java
+++ SpringMVC02/src/main/java/com/board/model/PagingVO.java
@@ -1,5 +1,10 @@
 package com.board.model;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import javax.servlet.http.HttpSession;
+
 import lombok.Data;
 /*페이징 처리 및 검색 기능을 모듈화하여
  * 	재사용 할 수 있도록하자
@@ -8,7 +13,7 @@ import lombok.Data;
 public class PagingVO {
 	//페이징 처리 관련 프로퍼티
 	private int cpage;//현재 보여줄 페이지 번호
-	private int pageSize;//한페이지당 보역줄목록 개수
+	private int pageSize=5;//한페이지당 보역줄목록 개수
 	private int totalCount;//총 게시글 수
 	private int pageCount;//페이지 수
 	
@@ -26,7 +31,12 @@ public class PagingVO {
 	private String findKeyword;//검색 키워드
 	
 	//페이징 처리 연산을 수행하는 메서드
-	public void init() {
+	public void init(HttpSession ses) {
+		if(ses!=null) {
+			ses.setAttribute("pageSize",pageSize);
+			
+		}
+		
 		pageCount=(totalCount-1)/pageSize+1;
 		if(cpage<1) {
 			cpage=1;
@@ -54,6 +64,58 @@ public class PagingVO {
 		 * */
 	}//-----------------------------------
 	
+	public String getPageNavi(String myctx,String loc,String userAgent) {
+		//myctx: 컨텍스트명, loc:"board/list", userAgent: 브라우저 종류 파악
+		//검색관련---
+		if(findType==null) {//검색어가 넘어오지 않을 경우
+			findType="";
+			findKeyword="";
+		}else {
+			//브라우저 IE일 경우 한글처리
+			if(userAgent.indexOf("MSIE")>-1||userAgent.indexOf("Trident")>-1) {
+				try {
+				findKeyword=URLEncoder.encode(findKeyword,"UTF-8");
+				}catch(UnsupportedEncodingException e) {
+					System.out.println(e);
+				}
+			}
+		}
+		
+		
+		String link=myctx+"/"+loc;
+		String qStr="?pageSize="+pageSize+"&findType="+findType+"&findKeyword="+findKeyword;
+		link+=qStr;
+		String str="";
+		StringBuilder buf=new StringBuilder();
+		buf.append("<ul class='pagination justify-content-center'>");
+		if(prevBlock>0) {
+			buf.append("<li class='page-item'>")
+				.append("<a class='page-link' href='"+link+"&cpage="+prevBlock+"'>")
+				.append("Prev")
+				.append("</a>")
+				.append("</li>");
+		}
+		for(int i=prevBlock+1;i<nextBlock && i<=pageCount;i++) {
+			String css=(i==cpage)?"active":"";
+			buf.append("<li class='page-item "+css+"'>");
+			buf.append("<a class='page-link' href='"+link+"&cpage="+i+"'>");
+			buf.append(i);
+			buf.append("</a>");
+			buf.append("</li>");
+		}
+		if(nextBlock<=pageCount) {
+			buf.append("<li class='page-item'>")
+				.append("<a class='page-link' href='"+link+"&cpage="+nextBlock+"'>")
+				.append("Next")
+				.append("</a>")
+				.append("</li>");
+		}
+		buf.append("</ul>");
+		str=buf.toString();
+		//System.out.println(str);
+		return str;
+	}
 	
 	
 }//////////////////////////////////////////////
+
