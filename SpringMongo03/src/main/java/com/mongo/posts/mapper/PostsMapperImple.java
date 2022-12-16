a92f89ede7c90cb50f70528011a0cd290af3c423
KimJinYeop <wlsduq1229@naver.com>
2022-12-16 17:57:08 +0900

    Commit 43
    멜론 차트를 가져와 Collection이름을 동적으로 생성하여(년월일) 차트를 저장. ajax를 이용하여 버튼을 2개를 통해 차트를 몽고디비에 저장, 저장된 목록을 출력하는 기능을 구현했다.

--- SpringMongo03/src/main/java/com/mongo/posts/mapper/PostsMapperImple.java
+++ SpringMongo03/src/main/java/com/mongo/posts/mapper/PostsMapperImple.java
@@ -7,10 +7,16 @@ import javax.annotation.Resource;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 import com.mongo.posts.domain.PostVO;
 import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+
+import static org.springframework.data.mongodb.core.query.Criteria.*;
+import static org.springframework.data.mongodb.core.query.Query.*;
+
 
 @Repository(value="postsMapperImpl")
 public class PostsMapperImple implements PostsMapper {
@@ -42,13 +48,19 @@ public class PostsMapperImple implements PostsMapper {
 
 	@Override
 	public int updatePosts(PostVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		//Update updateVO=Update.update("author", vo.getAuthor()).update("title", vo.getTitle());
+		Update uvo=new Update();
+		uvo.set("author", vo.getAuthor());
+		uvo.set("title", vo.getTitle());
+		UpdateResult res=mTemplate.updateFirst
+				(query(where("_id").is(vo.getId())),uvo,PostVO.class, "posts");
+		long n=res.getModifiedCount();
+		return (int)n;
 	}
 
 	@Override
 	public PostVO selectPosts(String id) {
-		PostVO vo=mTemplate.findOne(Query.query(Criteria.where("_id").is(id)), PostVO.class, "posts");
+		PostVO vo=mTemplate.findOne(query(where("_id").is(id)), PostVO.class, "posts");
 		return vo;
 	}
 
